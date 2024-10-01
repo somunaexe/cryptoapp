2ea26078135ded5a80b46f2f30b36a72c0c9efb1
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-10-02 00:54:38 +0100

    frontend mainly done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
+
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
+import './App.css'
 
 const App = () => {
   return (
     <div className="app">
         <div className="navbar">
-
+            <Navbar />
         </div>
         <div className="main">
-            <h1>Cryptbull</h1>
-        </div>
-        <div className="footer">
-
+            <Layout>
+                <div className='routes'>
+                    <Routes>
+                        <Route path="/" element={<Homepage />} />
+                        <Route path="/exchanges" element={<Exchanges />} />
+                        <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                        <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+                        <Route path="/news" element={<News />} />
+                    </Routes>
+                </div>
+            </Layout>
+            <div className="footer" >
+                <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                    Cryptbul <br />
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
+            </div>
         </div>
     </div>
   );
 }
 
-export default App
+export default App;

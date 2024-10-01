889a047ac9f7505fd6c93521e504aab2b1d14687
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-10-01 23:28:00 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
 const App = () => {
   return (
-    <div>
-      <h1>Cryptbull</h1>
+    <div className="app">
+        <div className="navbar">
+
+        </div>
+        <div className="main">
+            <h1>Cryptbull</h1>
+        </div>
+        <div className="footer">
+
+        </div>
     </div>
-  )
+  );
 }
 
 export default App

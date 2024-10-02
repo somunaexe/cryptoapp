fd519ee4b5dc58c59b4236ae6aa056a8de6ba00a
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-10-02 13:56:36 +0100

    setting up api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './app/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <Router>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </Router>
-    );
+);
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,12 @@
+ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+
+const cryptoApiHeaders = {
+
+}
+ // const options = {
+// 	method: 'GET',
+// 	hostname: 'coinranking1.p.rapidapi.com',
+// 	port: null,
+// 	path: '/stats?referenceCurrencyUuid=yhjMzLPhuIDl',
+// 	headers: {
+// };

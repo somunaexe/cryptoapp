1020a382577c9f6b511a98400decd42b5fd9e3cc
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-10-02 15:24:49 +0100

    added cryptocurrencies and news

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,36 @@ import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
 import icon from '../images/logo.png';
+
+const menuItems = [
+  {
+    key: '1',
+    icon: <HomeOutlined />,
+    label: <Link to="/">Home</Link>,
+  },
+  {
+    key: '2',
+    icon: <FundOutlined />,
+    label: <Link to="/cryptocurrencies">Cryptocurrencies</Link>,
+  },
+  {
+    key: '3',
+    icon: <MoneyCollectOutlined />,
+    label: <Link to="/exchanges">Exchanges</Link>,
+  },
+  {
+    key: '4',
+    icon: <BulbOutlined />,
+    label: <Link to="/news">News</Link>,
+  },
+];
+
+const MyMenu = () => {
+  return (
+    <Menu theme="dark" items={menuItems} />
+  );
+};
+
 const Navbar = () => {
   return (
     <div className='nav-container'>
@@ -12,7 +42,8 @@ const Navbar = () => {
         <Typography.Title level={2} className='logo'>
             <Link to="/">Cryptbull</Link>
         </Typography.Title>
-        <Menu theme="dark">
+        <MyMenu />
+        {/* <Menu theme="dark">
           <Menu.Item icon={<HomeOutlined />}>
             <Link to="/">Home</Link>
           </Menu.Item>
@@ -28,7 +59,7 @@ const Navbar = () => {
           <Menu.Item icon={<BulbOutlined />}>
             <Link to="/news">News</Link>
           </Menu.Item>
-        </Menu>
+        </Menu> */}
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './app/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
